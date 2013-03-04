adb4d92daab9db7512a173ee92b22e611afd4290
Reuven Yagel <robi@post.jce.ac.il>
2013-03-04 21:19:24 +0200

    with test file

---
+++ GitUploads.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Mvc;
+using GitUploads.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GitUploads.Tests
+{
+    [TestClass]
+    public class UploadControllerTests
+    {
+        [TestMethod]
+        public void CorrectMessageOnMainScreen()
+        {
+            //Arrange
+            var controller = new UploadController();
+
+            //Act
+            var result = controller.Index() as ViewResult;
+
+            //Assert
+            Assert.AreEqual("Please upload your file...", result.ViewBag.Message);
+        }
+    }
+}
